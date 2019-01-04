35bd4ecb180a81c6ea01efd258349d8fd6b86c0c
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-01-04 09:35:57 +0000

    Ready to code?

---
+++ src/main/java/OddNumberRemover.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+class OddNumberRemover {
+    List<Integer> removeOddNumbers(int... numbers) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/OddNumberRemoverTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OddNumberRemoverTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        OddNumberRemover oddNumberRemover = new OddNumberRemover();
+        assertThat(oddNumberRemover.removeOddNumbers( 1,2,3,4
+        )).containsExactly(2,4);
+    }
+}
